5e855876717b88d6a9f69eefc2141a67590df07e
Layan <layan6@gmail.com>
2026-07-19 19:53:53 +0300

    grid and shadow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,31 @@
 </svelte:head>
 
 <div
-    class="bg-[#fefefe] h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center"
+    class="bg-[#fefefe] min-h-[30rem] min-w-[20rem] rounded-3xl grid grid-cols-4 gap-1 p-6 pt-12 font-semibold text-2xl shadow-2xl shadow-cyan-800"
 >
-    <div class="bg-slate-400 h-20 w-20 rounded-full"></div>
+    <div
+        class="bg-blue-300 rounded-full col-span-4 h-16 flex items-center px-4 mb-3 text-yellow-50 text-3xl"
+    >
+        1 2 3
+    </div>
+    <button class="bg-blue-100">%</button>
+    <button class="bg-blue-100">√</button>
+    <button class="bg-blue-100">CE</button>
+    <button class="bg-blue-400 text-white">C</button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button class="bg-blue-200">-</button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button class="bg-blue-200">÷</button>
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button class="bg-blue-200">×</button>
+    <button>.</button>
+    <button>0</button>
+    <button class="bg-blue-100">=</button>
+    <button class="bg-blue-200">+</button>
 </div>
