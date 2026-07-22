47673a45708524a08489cffe8d13c73795c8b2cb
Layan <layan6@gmail.com>
2026-07-22 18:02:37 +0300

    evrything is working

---
+++ src/lib/icon/divisn.svelte
@@ -0,0 +1,13 @@
+ <!--div icon-->
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 24 24"
+        >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path
+                fill="currentColor"
+                d="M5 11h14v2H5zm7-3a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3m0 11a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3"
+            />
+        </svg>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,62 @@
+<script lang="ts">
+    import Divisn from "./../lib/icon/divisn.svelte";
+    import Minus from "./../lib/icon/minus.svelte";
+    import Squareroot from "./../lib/icon/squareroot.svelte";
+    import Backspace from "$lib/icon/backspace.svelte";
+
+    function addToEquation(value: string) {
+        equation += value;
+    }
+
+    function backspace() {
+        equation = equation.substring(0, equation.length - 1);
+    }
+    function clear() {
+        equation = "";
+    }
+
+   function solve() {
+    equation = eval(equation);
+   }
+    let equation: string = $state("");
+</script>
+
 <svelte:head>
     <title>calculator</title>
 </svelte:head>
 
 <div
-    class="bg-[#fefefe] min-h-[30rem] min-w-[20rem] rounded-3xl grid grid-cols-4 gap-1 p-6 pt-12 font-semibold text-2xl shadow-2xl shadow-cyan-800"
+    class="bg-[#04012e] min-h-[30rem] min-w-[20rem] rounded-3xl grid grid-cols-4 gap-1 p-6 pt-12 font-semibold text-2xl shadow-2xl shadow-black"
 >
     <div
-        class="bg-blue-300 rounded-full col-span-4 h-16 flex items-center px-4 mb-3 text-yellow-50 text-3xl"
+        class="bg-blue-200 rounded-full col-span-4 h-16 flex items-center px-4 mb-3 text-cyan-900 text-3xl"
     >
-        1 2 3
+        {equation}
     </div>
-    <button class="bg-blue-100">%</button>
-    <button class="bg-blue-100">√</button>
-    <button class="bg-blue-100">CE</button>
-    <button class="bg-blue-400 text-white">C</button>
-    <button>7</button>
-    <button>8</button>
-    <button>9</button>
-    <button class="bg-blue-200">-</button>
-    <button>4</button>
-    <button>5</button>
-    <button>6</button>
-    <button class="bg-blue-200">÷</button>
-    <button>1</button>
-    <button>2</button>
-    <button>3</button>
-    <button class="bg-blue-200">×</button>
+    <button onclick={() => addToEquation("/100")} class="bg-blue-50">%</button>
+    <button onclick={() => backspace()} class="bg-blue-50">
+        <Backspace />
+    </button>
+    <button class="bg-blue-50">CE</button>
+    <button onclick={() => clear()} class="bg-blue-400 text-white">C</button>
+    <button onclick={() => addToEquation("7")}>7</button>
+    <button onclick={() => addToEquation("8")}>8</button>
+    <button onclick={() => addToEquation("9")}>9</button>
+    <button onclick={() => addToEquation("-")} class="bg-blue-200">
+        <Minus />
+    </button>
+    <button onclick={() => addToEquation("4")}>4</button>
+    <button onclick={() => addToEquation("5")}>5</button>
+    <button onclick={() => addToEquation("6")}>6</button>
+    <button onclick={() => addToEquation("/")} class="bg-blue-200">
+        <Divisn />
+    </button>
+    <button onclick={() => addToEquation("1")}>1</button>
+    <button onclick={() => addToEquation("2")}>2</button>
+    <button onclick={() => addToEquation("3")}>3</button>
+    <button onclick={() => addToEquation("*")} class="bg-blue-200">×</button>
     <button>.</button>
-    <button>0</button>
-    <button class="bg-blue-100">=</button>
-    <button class="bg-blue-200">+</button>
+    <button onclick={() => addToEquation("0")}>0</button>
+    <button onclick={() => solve()} class="bg-blue-100">=</button>
+    <button onclick={() => addToEquation("+")} class="bg-blue-200">+</button>
 </div>
