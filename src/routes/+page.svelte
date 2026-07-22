dd96db32904244cefb3458b08cb9562fd2d650da
Layan <layan6@gmail.com>
2026-07-22 18:38:59 +0300

    finaly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import Divisn from "./../lib/icon/divisn.svelte";
     import Minus from "./../lib/icon/minus.svelte";
-    import Squareroot from "./../lib/icon/squareroot.svelte";
     import Backspace from "$lib/icon/backspace.svelte";
 
     function addToEquation(value: string) {
@@ -15,9 +14,9 @@
         equation = "";
     }
 
-   function solve() {
-    equation = eval(equation);
-   }
+    function solve() {
+        equation = eval(equation);
+    }
     let equation: string = $state("");
 </script>
 
@@ -37,26 +36,26 @@
     <button onclick={() => backspace()} class="bg-blue-50">
         <Backspace />
     </button>
-    <button class="bg-blue-50">CE</button>
+
     <button onclick={() => clear()} class="bg-blue-400 text-white">C</button>
+    <button onclick={() => addToEquation("-")} class="bg-blue-200">
+        <Minus />
+    </button>
     <button onclick={() => addToEquation("7")}>7</button>
     <button onclick={() => addToEquation("8")}>8</button>
     <button onclick={() => addToEquation("9")}>9</button>
-    <button onclick={() => addToEquation("-")} class="bg-blue-200">
-        <Minus />
+    <button onclick={() => addToEquation("/")} class="bg-blue-200">
+        <Divisn />
     </button>
     <button onclick={() => addToEquation("4")}>4</button>
     <button onclick={() => addToEquation("5")}>5</button>
     <button onclick={() => addToEquation("6")}>6</button>
-    <button onclick={() => addToEquation("/")} class="bg-blue-200">
-        <Divisn />
-    </button>
+    <button onclick={() => addToEquation("*")} class="bg-blue-200">×</button>
     <button onclick={() => addToEquation("1")}>1</button>
     <button onclick={() => addToEquation("2")}>2</button>
     <button onclick={() => addToEquation("3")}>3</button>
-    <button onclick={() => addToEquation("*")} class="bg-blue-200">×</button>
-    <button>.</button>
-    <button onclick={() => addToEquation("0")}>0</button>
-    <button onclick={() => solve()} class="bg-blue-100">=</button>
     <button onclick={() => addToEquation("+")} class="bg-blue-200">+</button>
+    <button onclick={() => addToEquation(".")}>.</button>
+    <button onclick={() => addToEquation("0")}>0</button>
+    <button onclick={() => solve()} class="bg-blue-100 col-span-2">=</button>
 </div>
